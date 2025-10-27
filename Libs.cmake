e1580d2c68f5d46534237f0f7f902bf35a02787d
rhackerd <josefduda18@gmail.com>
2025-10-27 19:10:27 +0100

    Added Nova-Logger@1.0.4 support, added brun.sh script, and automatic file search when building

--- Libs.cmake
+++ Libs.cmake
@@ -1,2 +1,2 @@
-CPMAddPackage("gh:rhackerd/Nova-Log@1.0.3")
+CPMAddPackage("gh:rhackerd/Nova-Log@1.0.4")
 CPMAddPackage("gh:fmtlib/fmt#12.0.0")
--- src/core/core.cpp
+++ src/core/core.cpp
@@ -1,16 +1,13 @@
 #include "include/core.h"
-#include <nova/logger/logger.hpp>
-
-Core::Core() {
-    Nova::Logger::init("Template-Cmake");
+Core::Core(): _l("Cmake Template") {
 }
 
 Core::~Core() {}
 
 void Core::load() {
-    Nova::Logger::info("Core loaded.");
+    _l.info("Core loaded.");
 }
 
 void Core::unload() {
-    Nova::Logger::info("Core unloaded");
+    _l.info("Core unloaded");
 }
